e15d1cd8a09f9269d1ffe7918b9b4b2756cd1986
GuDMuller <gustavomuller6@gmail.com>
2022-11-12 02:34:15 -0300

    Commit final

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GlobalSolution.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
